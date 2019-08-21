29ee7f0b56147a251fb1fd075bc7700a0cd04bbe
Michael.cheng <chylove1982@hotmail.com>
2019-08-21 18:00:25 +0800

    ok

--- src/Module/CodeGenerator/UI/module-code-generator/src/config/index.js
+++ src/Module/CodeGenerator/UI/module-code-generator/src/config/index.js
@@ -1,11 +1,11 @@
 const isDev = process.env.NODE_ENV !== 'production'
 
 const config = {
-  baseUrl: ''
+  baseUrl: 'http://49.235.222.38:6220/api/'
 }
 
 // 开发模式
 if (isDev) {
-  config.baseUrl = 'http://localhost:6220/api/'
+  config.baseUrl = 'http://49.235.222.38:6220/api/'
 }
 export default config
